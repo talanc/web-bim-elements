64527240ab3ff86337c734b2cfdb3325beb8977f
talanc <talanc@users.noreply.github.com>
2024-04-07 14:54:48 +1000

    aaa

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,46 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import * as Models from './models';
 
+// Z up
+THREE.Object3D.DEFAULT_UP = new THREE.Vector3(0, 0, 1);
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color(0xf0f0f0);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const controls = new OrbitControls(camera, renderer.domElement);
 
-camera.position.z = 5;
+scene.add(new THREE.AmbientLight(0xffffff));
+
+const light = new THREE.DirectionalLight(0xffffff, 1.5);
+light.position.set(3, 3, 3);
+scene.add(light);
+
+const material = new THREE.MeshLambertMaterial({ color: 0xff00ff });
+
+const axesHelper = new THREE.AxesHelper(1);
+scene.add(axesHelper);
+
+camera.position.set(-3, -5, 5);
 
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+  controls.update();
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
 animate();
+
+//
+// Input
+//
 
 const shedInput: Models.ShedInput = {
   user: {
@@ -35,9 +51,37 @@ const shedInput: Models.ShedInput = {
     pitch: 22
   },
   calc: {
-    column: Models.createC("C15024", 152,64, 15.5, 2.4),
+    column: Models.createC("C15024", 152, 64, 15.5, 2.4),
     sideGirt: Models.createTH("TH64", 64, 100, 1),
   }
 }
+
+//
+// Output
+//
 const shedBim = Models.createShedBim(shedInput);
 console.log(shedBim);
+
+//
+// Model
+//
+
+const allColumns = [...shedBim.columnsLeft, ...shedBim.columnsRight];
+
+for (const column of allColumns) {
+  const shape = Models.createShape(column.mat);
+  const settings = {
+    depth: column.line.distance() / 1000,
+    bevelEnabled: false,
+  };
+  const geometry = new THREE.ExtrudeGeometry(shape, settings);
+  const mesh = new THREE.Mesh(geometry, material);
+
+  let mat4 = new THREE.Matrix4();
+  mat4.setPosition(column.line.start.clone().divideScalar(1000));
+  mat4.multiply(column.trans);
+
+  mesh.applyMatrix4(mat4);
+
+  scene.add(mesh);
+}
--- src/models.ts
+++ src/models.ts
@@ -1,8 +1,14 @@
+import { Line3, Matrix4, Shape, Vector2, Vector3 } from 'three';
+
+// Core
+
+export const rad90: number = Math.PI / 2;
+export const rad180: number = Math.PI;
+export const rad270: number = Math.PI + Math.PI / 2;
+
 //
 // Elements
 //
-
-import { Line3, Matrix3, Vector3 } from 'three';
 
 export class ElemMaterial {
     name = "";
@@ -18,7 +24,7 @@ export class ElemMaterial {
         return this.width;
     }
     lip = 0;
-    
+
     // th / z
     get depth(): number {
         return this.height;
@@ -27,7 +33,7 @@ export class ElemMaterial {
 
 export class ElemSection {
     line!: Line3;
-    trans!: Matrix3;
+    trans!: Matrix4;
     mat!: ElemMaterial;
 }
 
@@ -89,15 +95,19 @@ class ShedBimBuilder {
                 y -= calc.column.flange / 2;
             }
 
+            const rot = i == 0 ? rad270 : rad90;
+
             const leftColumn = new ElemSection();
             leftColumn.line = new Line3(new Vector3(x1, y, z1), new Vector3(x1, y, z2));
-            leftColumn.trans = new Matrix3();
+            leftColumn.trans = new Matrix4();
+            leftColumn.trans.makeRotationZ(rot);
             leftColumn.mat = calc.column;
             bim.columnsLeft.push(leftColumn);
 
             const rightColumn = new ElemSection();
             rightColumn.line = new Line3(new Vector3(x2, y, z1), new Vector3(x2, y, z2));
-            rightColumn.trans = new Matrix3();
+            rightColumn.trans = new Matrix4();
+            rightColumn.trans.makeRotationZ(rot);
             rightColumn.mat = calc.column;
             bim.columnsRight.push(rightColumn);
         }
@@ -129,4 +139,50 @@ export function createTH(name: string, depth: number, width: number, thickness: 
     th.width = width;
     th.thickness = thickness;
     return th;
+}
+
+export function V2(x: number, y: number) {
+    return new Vector2(x, y);
+}
+
+export function V3(x: number, y: number, z: number) {
+    return new Vector3(x, y, z);
+}
+
+export function createShape(mat: ElemMaterial) {
+    const hw = mat.flange / 2 / 1000;
+    const hh = mat.web / 2 / 1000;
+    const l = mat.lip / 1000;
+    const t = mat.thickness / 1000;
+
+    const x1 = -hw;
+    const x2 = -hw + t;
+    const x3 = +hw - t;
+    const x4 = +hw;
+
+    const y1 = -hh;
+    const y2 = -hh + t;
+    const y3 = -hh + l;
+    const y4 = +hh - l;
+    const y5 = +hh - t;
+    const y6 = +hh;
+
+    const pts = [
+        V2(x1, y1),
+        V2(x4, y1),
+        V2(x4, y3),
+        V2(x3, y3),
+        V2(x3, y2),
+        V2(x2, y2),
+        V2(x2, y5),
+        V2(x3, y5),
+        V2(x3, y4),
+        V2(x4, y4),
+        V2(x4, y6),
+        V2(x1, y6),
+    ];
+
+    const shape = new Shape(pts);
+
+    return shape;
 }
